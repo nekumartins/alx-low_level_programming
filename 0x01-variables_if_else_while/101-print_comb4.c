49501562a7b1283063fe3a860c1a8324f0d20410
Riley <akpotohwoo@gmail.com>
2023-06-20 05:52:43 +0100

    task 11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i != j && i - j < 0)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
