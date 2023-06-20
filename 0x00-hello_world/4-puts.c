43c2cf4962077fb519e7055227b12ed1e1fad197
Riley <akpotohwoo@gmail.com>
2023-06-20 01:11:54 +0100

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- *		puzzle, followed by a new line.
+ *		puzzle", followed by a new line.
  *
  * Return:0.
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle");
 
 			return (0);
 }
