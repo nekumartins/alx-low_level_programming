3d05265efa3e1cab8f4e9f63b631f2dd49372da9
Riley <akpotohwoo@gmail.com>
2023-07-06 03:34:19 +0100

    task 5 commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return square root
+ * @n: number
+ *
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	int root = _sqrt_recursion(n - 1);
+
+	if (root * root > n)
+	{
+		return (root - 1);
+	}
+
+	return (root);
+	}
+
+
