be0a1b1126effd1678dc9abe7a6bdc47fc387d6b
Riley <akpotohwoo@gmail.com>
2023-07-06 03:10:22 +0100

    task 2 commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of string
+ * @s: pointer to string
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
