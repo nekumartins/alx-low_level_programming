210148042e31d2684289834e412a190b41e15bb4
Riley <akpotohwoo@gmail.com>
2023-07-06 03:50:54 +0100

    task 5 update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * test - check for square root
+ * @n: number
+ * @i: possible values
+ *
+ * Return: -1 or square root
+ */
+
+int test(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (test(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - return square root
  * @n: number
  *
@@ -8,26 +25,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	int root;
-
 	if (n < 0)
-	{
 		return (-1);
-	}
-
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-
-	root = _sqrt_recursion(n - 1);
-
-	if (root * root > n)
-	{
-		return (root - 1);
-	}
-
-	return (root);
-	}
+	return (check_sqrt(n, 0));
+}
 
 
