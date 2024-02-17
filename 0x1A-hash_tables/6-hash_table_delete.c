14dd7c42e60d44442cf3a62b46a71eac9d2a6d79
Chukwuneku <akpotohwoo@gmail.com>
2024-02-17 05:52:38 +0100

    update 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
-* hash_table_delete - Deletes a hash table.
-* @ht: hash table.
+* hash_table_delete - Deletes hash table.
+* @ht: A pointer to hash table.
 */
 void hash_table_delete(hash_table_t *ht)
 {
